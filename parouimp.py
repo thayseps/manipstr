abf3a73e3bce3606669b8d5a9c06fee18d8a422e
Thayse <78800993+thayseps@users.noreply.github.com>
2023-03-10 17:21:33 -0300

    Delete parouimp.py

--- parouimp.py
+++
@@ -1,7 +0,0 @@
-num = int(input('Me diga um número qualquer: '))
-resultado = num % 2
-if resultado == 0:
-    print('O número {} é par!'.format(num))
-else:
-    print('O número {} é ímpar!'.format(num))
-  
